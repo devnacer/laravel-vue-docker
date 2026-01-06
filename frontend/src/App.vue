8ef58807f7da900e930f92ab81ff2fafcef23428
devnacer <kalache.nacer.kn@gmail.com>
2026-01-06 22:41:48 +0000

    Initial commit: Laravel + Vue + Docker setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const message = ref('Loading...')
+
+onMounted(async () => {
+  const res = await fetch('http://10.145.231.81:4555/api/hello')  
+  const data = await res.json()
+  message.value = data.message + ' (' + data.container + ')'
+})
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
